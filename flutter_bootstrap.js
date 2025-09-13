f7e87c620afecab5ebde80ddfa6e0d329c35fd81
zmtzawqlp <zmtzawqlp@users.noreply.github.com>
2025-09-13 05:21:51 +0000

    Deploying to gh-pages from @ fluttercandies/JsonToDart@ba9a73732e42d6eea37b4c33508072dbdd4d0f7a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f7f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2b7","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "156818530"
+    serviceWorkerVersion: "1778762756"
   }
 });
